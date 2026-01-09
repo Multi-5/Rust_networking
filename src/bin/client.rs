65eb4eca75b1bf492feda48a44f674e55bf3b0eb
Stephan <schroth.stephan@web.de>
2026-01-09 09:52:37 +0100

    initial structure

---
+++ src/bin/client.rs
@@ -0,0 +1,6 @@
+#[tokio::main]
+async fn main() -> Result<(), i32> {
+    println!("I'm the client!");
+    //client::start_client().await;
+    Ok(())
+}
---
+++ src/bin/server.rs
@@ -0,0 +1,6 @@
+#[tokio::main]
+async fn main() -> Result<(), i32> {
+    //server::start_server().await;
+    println!("I'm the server");
+    Ok(())
+}
